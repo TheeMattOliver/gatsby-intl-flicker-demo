90f2c492c938ca7a7506e224276803ec364fe613
Matt Oliver <mattoliver.mattoliver@gmail.com>
2021-07-01 07:48:30 -0500

    wip :construction:

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import { FormattedMessage } from "gatsby-plugin-intl"
 import Layout from "../components/Layout"
 import BigTitle from "../components/BigTitle"
+import SEO from "../components/SEO"
 
 
 export default function Home() {
